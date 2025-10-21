a7e6750d9510c212a11c86ddea3d7920cd8482ff
Rifushigi <rifushigi.teda@gmail.com>
2025-10-21 19:32:20 +0100

    feat: add LLMService interface for query parsing

---
+++ src/main/java/com/rifushigi/stringly/service/LLMService.java
@@ -0,0 +1,13 @@
+package com.rifushigi.stringly.service;
+
+import com.rifushigi.stringly.dto.ParseResult;
+
+public interface LLMService {
+    /**
+    * Parse a natural language query into filter parameters
+    * @param query The natural language query
+    * @return Map of filter parameters or null if parsing fails
+    * @throws Exception if the LLM service is unavailable or returns an error
+    **/
+    ParseResult parseQuery(String query) throws Exception;
+}
