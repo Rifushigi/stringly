e7c4f8c9a7478b6b4219618dc08a683e5934a739
Rifushigi <rifushigi.teda@gmail.com>
2025-10-21 14:10:27 +0100

    feat: implement StringAnalysisRepository with basic CRUD and filter methods

---
+++ src/main/java/com/rifushigi/stringly/repository/StringAnalysisRepository.java
@@ -0,0 +1,50 @@
+package com.rifushigi.stringly.repository;
+
+import com.rifushigi.stringly.entity.StringAnalysis;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class StringAnalysisRepository {
+
+    private final Map<String, StringAnalysis> storage = new ConcurrentHashMap<>();
+
+    public StringAnalysis save (StringAnalysis analysis) {
+        storage.put(analysis.getId(), analysis);
+        return analysis;
+    }
+
+    public Optional<StringAnalysis> findById(String id){
+        return Optional.ofNullable(storage.get(id));
+    }
+
+    public List<StringAnalysis> findAll(){
+        return storage.values().stream().toList();
+    }
+
+    public boolean existsById(String id){
+        return storage.containsKey(id);
+    }
+
+    public void deleteById(String id){
+        storage.remove(id);
+    }
+
+    public void deleteAll(){
+        storage.clear();
+    }
+
+    public List<StringAnalysis> findWithFilters(Boolean isPalindrome, Integer minLength,
+                                                Integer maxLength, Integer wordCount, String containsCharacter){
+        return storage.values().stream()
+                .filter(s -> isPalindrome == null || s.getIsPalindrome().equals(isPalindrome))
+                .filter(s -> minLength == null || s.getLength() >= minLength)
+                .filter(s -> maxLength == null || s.getLength() <= maxLength)
+                .filter(s -> wordCount == null || s.getWordCount().equals(wordCount))
+                .filter(s -> containsCharacter == null || s.getValue().toLowerCase().contains(containsCharacter.toLowerCase()))
+                .toList();
+    }
+}
