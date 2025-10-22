2103c78705d929d01a2934f6eaa9f5d5c7fb75f3
Rifushigi <rifushigi.teda@gmail.com>
2025-10-22 23:53:19 +0100

    feat: add no-args constructor to StringAnalysis entity

--- src/main/java/com/rifushigi/stringly/entity/StringAnalysis.java
+++ src/main/java/com/rifushigi/stringly/entity/StringAnalysis.java
@@ -3,11 +3,13 @@ package com.rifushigi.stringly.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class StringAnalysis {
 
