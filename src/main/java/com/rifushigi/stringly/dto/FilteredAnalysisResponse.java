458e7c735917e2ea162aa9e7a0a52465ae9aca46
Rifushigi <rifushigi.teda@gmail.com>
2025-10-22 14:52:17 +0100

    refactor: update FilteredAnalysisResponse to use StringAnalysisResponse for data field

--- src/main/java/com/rifushigi/stringly/dto/FilteredAnalysisResponse.java
+++ src/main/java/com/rifushigi/stringly/dto/FilteredAnalysisResponse.java
@@ -1,13 +1,10 @@
 package com.rifushigi.stringly.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.rifushigi.stringly.entity.StringAnalysis;
-
 import java.util.List;
-import java.util.Map;
 
 public record FilteredAnalysisResponse(
-        List<StringAnalysis> data,
+        List<StringAnalysisResponse> data,
         String count,
         @JsonProperty(value = "filters_applied")
         ParseResult filtersApplied
