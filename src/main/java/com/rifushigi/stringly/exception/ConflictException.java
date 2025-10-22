518f25e65f8b98f1624d13279ab0a620b4391e80
Rifushigi <rifushigi.teda@gmail.com>
2025-10-22 16:03:20 +0100

    feat: add ConflictException for handling conflict scenarios

---
+++ src/main/java/com/rifushigi/stringly/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.rifushigi.stringly.exception;
+
+public class ConflictException extends RuntimeException{
+    public ConflictException(String message){
+        super(message);
+    }
+}
